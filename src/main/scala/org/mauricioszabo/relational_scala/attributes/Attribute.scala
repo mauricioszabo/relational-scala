66f5922f78c291d1ed3d2e5d9f7ccfd4183b0000
Maurício Szabo <mauricio.szabo@gmail.com>
2013-08-06 10:49:30 -0300

    Added Attribute class.

---
+++ src/main/scala/org/mauricioszabo/relational_scala/attributes/Attribute.scala
@@ -0,0 +1,32 @@
+package org.mauricioszabo.relational_scala.attributes
+
+import org.mauricioszabo.relational_scala.comparissions._
+
+trait Attribute {
+  def representation: String
+
+  def ==(other: => Any ) = new Equality("=", this, other)
+  def !=(other: => Any ) = new Equality("<>", this, other)
+  def <=(other: Any) = new Equality("<=", this, other)
+  def <(other: Any) = new Equality("<", this, other)
+  def >=(other: Any) = new Equality(">=", this, other)
+  def >(other: Any) = new Equality(">", this, other)
+
+  def isNull = this == null
+
+  def =~(other: Any) = like(other)
+  def like(other: Any) = new Equality("LIKE", this, other)
+  def !~(other: Any) = notLike(other)
+  def notLike(other: Any) = new Equality("NOT LIKE", this, other)
+
+  def in(list: Seq[Any]) = new In(this, list)
+
+  def sum = new Function("SUM", this)
+  def avg = new Function("AVG", this)
+  def min = new Function("MIN", this)
+  def max = new Function("MAX", this)
+  def upper = new Function("UPPER", this)
+  def lower = new Function("LOWER", this)
+  def length = new Function("LENGTH", this)
+  def count = new Function("COUNT", this)
+}

54b58400dfc5cd6acbce998fb406bca8e2921544
Maurício Szabo <mauricio.szabo@gmail.com>
2013-08-19 14:56:46 -0300

    Added orders to Partial.

--- src/main/scala/org/mauricioszabo/relational_scala/Partial.scala
+++ src/main/scala/org/mauricioszabo/relational_scala/Partial.scala
@@ -1,5 +1,10 @@
 package org.mauricioszabo.relational_scala
 
+import org.mauricioszabo.relational_scala.orders._
+
 trait Partial {
   def partial: PartialStatement
+
+  def asc = new Ascending(this)
+  def desc = new Descending(this)
 }

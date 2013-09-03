ef2fcf186fdd5cb349299634ed329b9c0a5eb911
Maurício Szabo <mauricio.szabo@gmail.com>
2013-09-03 20:17:37 -0300

    Added "float" (and equals) to NumericAttribute.

--- src/main/scala/org/mauricioszabo/relational_scala/results/NumericAttribute.scala
+++ src/main/scala/org/mauricioszabo/relational_scala/results/NumericAttribute.scala
@@ -2,9 +2,15 @@ package org.mauricioszabo.relational_scala.results
 
 import scala.math.ScalaNumericAnyConversions
 
-class NumericAttribute[A <% ScalaNumericAnyConversions](number: A) extends Attribute(number) {
+class NumericAttribute[A <% ScalaNumericAnyConversions](protected val number: A) extends Attribute(number) {
   override def as(p: Int.type) = number.toInt
+  override def as(p: Float.type) = number.toFloat
   override def as(p: Double.type) = number.toDouble
   override def as(p: Long.type) = number.toLong
   override def as(p: Short.type) = number.toShort
+
+  override def equals(other: Any) = other match {
+    case n: NumericAttribute[A] => number == n.number
+    case _ => false
+  }
 }

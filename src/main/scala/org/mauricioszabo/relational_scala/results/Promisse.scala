b35dacc9ebcb4b3ae4c2d8b4cc2ce33e8d81127a
Maurício Szabo <mauricio.szabo@gmail.com>
2013-09-03 20:18:23 -0300

    Corrected "Promisse" with new Attributes api.

--- src/main/scala/org/mauricioszabo/relational_scala/results/Promisse.scala
+++ src/main/scala/org/mauricioszabo/relational_scala/results/Promisse.scala
@@ -1,7 +1,12 @@
 package org.mauricioszabo.relational_scala.results
 
 class Promisse(resultSet: java.sql.ResultSet) extends Traversable[Attributes] {
-  def foreach[U](fn: Attributes => U) {
-    while(resultSet.next) fn(new Attributes(resultSet))
+  def foreach[U](fn: Attributes => U) = while(resultSet.next) {
+    val meta = resultSet.getMetaData
+    val columns = 1.to(meta.getColumnCount).map { i =>
+      val obj = resultSet.getObject(i).asInstanceOf[Any]
+      Symbol(meta.getColumnName(i)) -> Attribute(obj)
+    }
+    fn(new Attributes(columns))
   }
 }

498a1578eb441253938f734db981e89297e12c8f
Maurício Szabo <mauricio.szabo@gmail.com>
2013-09-03 20:18:46 -0300

    Added Dynamic to TableLike.

--- src/main/scala/org/mauricioszabo/relational_scala/tables/TableLike.scala
+++ src/main/scala/org/mauricioszabo/relational_scala/tables/TableLike.scala
@@ -1,13 +1,15 @@
 package org.mauricioszabo.relational_scala.tables
 
 import org.mauricioszabo.relational_scala._
+import scala.language.dynamics
 
-trait TableLike extends Partial {
+trait TableLike extends Partial with Dynamic {
   def as: Alias = as(Random.generate(5))
   def as(name: String): Alias
 
   def representation: String
 
+  def selectDynamic(field: String) = apply(field)
   def apply(attribute: Symbol): attributes.Attribute = this(attribute.name)
   def apply(attribute: String): attributes.Attribute
 

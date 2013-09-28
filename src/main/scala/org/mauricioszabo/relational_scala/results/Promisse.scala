40caf9ce9fbf4a766cc21d59e12b33cff4ce2310
Maurício Szabo <mauricio.szabo@gmail.com>
2013-09-28 10:40:19 -0300

    Removed some code from Promisse and moved to FullSelect.

--- src/main/scala/org/mauricioszabo/relational_scala/results/Promisse.scala
+++ src/main/scala/org/mauricioszabo/relational_scala/results/Promisse.scala
@@ -3,10 +3,13 @@ package org.mauricioszabo.relational_scala.results
 class Promisse(resultSet: java.sql.ResultSet) extends Traversable[Attributes] {
   def foreach[U](fn: Attributes => U) = while(resultSet.next) {
     val meta = resultSet.getMetaData
-    val columns = 1.to(meta.getColumnCount).map { i =>
-      val obj = resultSet.getObject(i).asInstanceOf[Any]
-      Symbol(meta.getColumnName(i)) -> Attribute(obj)
+    val columns = 1.to(meta.getColumnCount).foldLeft(Map[Symbol, Attribute]()) { (map, i) =>
+      val columnName = Symbol(meta getColumnName i)
+      map get columnName match {
+        case Some(_) => map
+        case None => map + (columnName -> Attribute(resultSet.getObject(i).asInstanceOf[Any]))
+      }
     }
-    fn(new Attributes(Map(columns: _*)))
+    fn(new Attributes(columns))
   }
 }

567dd9f37f95500a75ec00abc3f2d42006cccb8e
Maurício Szabo <mauricio.szabo@gmail.com>
2014-09-15 17:51:49 -0300

    Better way of doing comparissions (OR and AND).

--- src/main/scala/relational/comparissions/Comparission.scala
+++ src/main/scala/relational/comparissions/Comparission.scala
@@ -4,15 +4,21 @@ import relational._
 import relational.comparissions._
 
 trait Comparission extends attributes.Comparable {
-  protected def present = true
-
   def unary_! : Comparission = new Not(this)
 
-  def ||(other: Comparission): Comparission =
-    if(other.present) new Or(List(this, other))
-    else this
+  def ||(other: Comparission): Comparission = (this, other) match {
+    case (_, None) => this
+    case (Or(a1), Or(a2)) => Or(a1 ++ a2)
+    case (_, Or(attributes)) => Or(this +: attributes)
+    case (Or(attributes), _) => Or(attributes :+ other)
+    case _ => new Or(Vector(this, other))
+  }
 
-  def &&(other: Comparission): Comparission =
-    if(other.present) new And(List(this, other))
-    else this
+  def &&(other: Comparission): Comparission = (this, other) match {
+    case (_, None) => this
+    case (And(a1), And(a2)) => And(a1 ++ a2)
+    case (_, And(attributes)) => And(this +: attributes)
+    case (And(attributes), _) => And(attributes :+ other)
+    case _ => new And(Vector(this, other))
+  }
 }
--- src/main/scala/relational/comparissions/None.scala
+++ src/main/scala/relational/comparissions/None.scala
@@ -3,8 +3,6 @@ package relational.comparissions
 import relational.PartialStatement
 
 object None extends Comparission {
-  override protected def present = false
-
   lazy val partial = new PartialStatement("", Nil)
 
   override def ||(other: Comparission) = other

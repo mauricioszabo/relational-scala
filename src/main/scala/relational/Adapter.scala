e1ebf98b0ab35124a7d95d7a02e8d0a097fcf80e
Maurício Szabo <mauricio.szabo@gmail.com>
2014-09-17 22:01:21 -0300

    Removed code from Adapter.

--- src/main/scala/relational/Adapter.scala
+++ src/main/scala/relational/Adapter.scala
@@ -10,69 +10,13 @@ class Adapter(d: Symbol) {
 
   def configure(driver: Symbol = DEFAULT_DRIVER) {
     this.driver = driver
-    attributes.ConfigFunctions
   }
 
   def currentDriver = driver
-
-  def getFunction(name: Symbol) =
-    Adapter.getFunctionForDriver(driver, name)
 }
 
 object Adapter extends Adapter {
   implicit val default = {
     this
-  }
-
-  type Function = (Partial, Seq[Partial]) => (String, Seq[Any])
-  type ListOfFunctions = (Symbol, Function)
-  type HashMapType = HashMap[Symbol, Function] with SynchronizedMap[Symbol, Function]
-  private val functions = new HashMap[Symbol, HashMapType] with SynchronizedMap[Symbol, HashMapType]
-
-  def defineFunctionN(name: Symbol, strings: (Symbol, String)*) {
-    val functions = strings.map { case(driver, string) =>
-      val function = { (self: Partial, params: Seq[Partial]) =>
-        val partialsList = self :: params.toList
-        val matchList = """\$(\d+)""".r.findAllMatchIn(string)
-
-        matchList.foldLeft(string -> List[Any]()) { case((query, attributes), matchElement) =>
-          val index = matchElement.group(1).toInt
-          val partial = partialsList(index).partial
-
-          val q = query.replaceAll("\\$" + index, partial.query)
-          val a = attributes ++ partial.attributes
-          (q, a)
-        }
-      }
-      (driver, function)
-    }
-    defineFunction(name, functions: _*)
-  }
-
-  def defineFunction(name: Symbol, functions: ListOfFunctions*) {
-    functions.foreach { case(driver, body) =>
-      addCustomFunction(name, driver, body)
-    }
-  }
-
-  def addCustomFunction(name: Symbol, driver: Symbol, function: Function) {
-    val entry = getDriverEntry(driver)
-    entry(name) = function
-  }
-
-  protected def getFunctionForDriver(driver: Symbol, name: Symbol): Function = {
-    getDriverEntry(driver) get name match {
-      case Some(function) => function
-      case None if(driver == DEFAULT_DRIVER) => { (p: Partial, a: Seq[Partial]) => ("", Nil) }
-      case None => getFunctionForDriver(DEFAULT_DRIVER, name)
-    }
-  }
-
-  private def getDriverEntry(driver: Symbol) = functions get driver match {
-    case Some(entry) => entry
-    case None =>
-      val entry = new HashMap[Symbol, Function] with SynchronizedMap[Symbol, Function]
-      functions(driver) = entry
-      entry
   }
 }
--- src/test/scala/relational/AdapterTest.scala
+++ src/test/scala/relational/AdapterTest.scala
@@ -7,49 +7,4 @@ import relational._
 class AdapterTest extends WordSpec with matchers.ShouldMatchers with BeforeAndAfterEach {
   implicit val adapter = new Adapter
   adapter.configure('all)
-
-  lazy val table = new tables.Table("examples")
-
-  class Test extends attrs.Attribute(table, "name") {
-    def foobar(param: Any) =
-      new attrs.Function('foobar, this, attrs.Attribute.wrap(param))
-
-    def foobar2(p1: Any, p2: Any) =
-      new attrs.Function('foobar2, this, attrs.Attribute.wrap(p1), attrs.Attribute.wrap(p2))
-  }
-
-  lazy val name = new Test
-
-  "Adapter" should {
-    "create a function in SQL" in {
-      Adapter.defineFunction('foobar, 'all -> { (p, params) =>
-        val param = params.head
-
-        (
-          "FOO_BAR(" + p.partial.query + ", "+ param.partial.query + ")",
-          p.partial.attributes.toList ++ param.partial.attributes
-        )
-      })
-
-      name.foobar(10).partial.toPseudoSQL should be === "FOO_BAR(\"examples\".\"name\", 10)"
-    }
-
-    "create a function in a simpler way" in {
-      Adapter.defineFunctionN('foobar, 'all -> "FOOBAR($0, $1)")
-      Adapter.defineFunctionN('foobar2, 'all -> "$0 FOOBAR($2, $1)")
-
-      name.foobar(10).partial.toPseudoSQL should be === "FOOBAR(\"examples\".\"name\", 10)"
-      name.foobar2(10, 20).partial.toPseudoSQL should be === "\"examples\".\"name\" FOOBAR(20, 10)"
-    }
-
-    "create a method that has different behaviour for each adapter" in {
-      Adapter.defineFunctionN('foobar, 'all -> "$0 FOO($1)", 'oracle -> "$0 BAR($1)")
-
-      adapter configure 'mysql
-      name.foobar(10).partial.toPseudoSQL should be === "\"examples\".\"name\" FOO(10)"
-
-      adapter configure 'oracle
-      name.foobar(10).partial.toPseudoSQL should be === "\"examples\".\"name\" BAR(10)"
-    }
-  }
 }

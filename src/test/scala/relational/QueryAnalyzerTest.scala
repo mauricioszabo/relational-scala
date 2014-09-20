fab2e4b22477a3d175cd98743eca8cc7e2e7fd9c
Maurício Szabo <mauricio.szabo@gmail.com>
2014-09-20 15:42:24 -0300

    Added QueryAnalyzer.

---
+++ src/test/scala/relational/QueryAnalyzerTest.scala
@@ -0,0 +1,34 @@
+package test.relational
+
+import org.scalatest._
+import relational.tables.Table
+import relational.attributes.Attribute
+import relational.joins._
+import relational.{QueryAnalyzer, Selector}
+import relational.clauses.Select
+
+class QueryAnalyzerTest extends WordSpec with matchers.ShouldMatchers {
+  val ex = new Table("examples")
+  val joined = new Table("joined")
+  val aux = new Table("auxiliary")
+
+  "QueryAnalyzer" should {
+    "show which columns are the same in the query" in {
+      val selector = Selector(
+        select=Select.select(ex.id, joined.ex_id, ex.name, ex.alt_name, ex.obj, ex.obj2, ex.single),
+        from=List(ex),
+        join=List(InnerJoin(joined, ex.id == joined.ex_id)),
+        where=(ex.name == ex.alt_name),
+        having=(ex.obj2 == ex.obj)
+      )
+      val analyzer = new QueryAnalyzer(selector)
+
+      analyzer.fields should be === Set(
+        Set(ex.id, joined.ex_id),
+        Set(ex.name, ex.alt_name),
+        Set(ex.obj, ex.obj2),
+        Set(ex.single)
+      )
+    }
+  }
+}

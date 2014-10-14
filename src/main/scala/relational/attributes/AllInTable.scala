f45b5483d16e85a87f3e1812d0bcc6e2d76e3983
Maurício Szabo <mauricio.szabo@gmail.com>
2014-10-14 11:39:47 -0300

    Changed AllInTable to be a case class.

--- src/main/scala/relational/attributes/AllInTable.scala
+++ src/main/scala/relational/attributes/AllInTable.scala
@@ -3,7 +3,7 @@ package relational.attributes
 import relational.PartialStatement
 import relational._
 
-class AllInTable(table: tables.TableLike) extends AttributeLike {
+case class AllInTable(table: tables.TableLike) extends AttributeLike {
   lazy val partial = PartialStatement { a =>
     table.representation(a) + ".*" -> Nil
   }

316dde81b472e4359bca46e9e2c3042c30f2b472
Maurício Szabo <mauricio.szabo@gmail.com>
2014-10-14 11:23:26 -0300

    Helper now uses an implicit Adapter.

--- src/test/scala/relational/Helpers.scala
+++ src/test/scala/relational/Helpers.scala
@@ -3,9 +3,11 @@ package test.relational
 import org.scalatest.matchers._
 import relational.attributes._
 import relational.tables._
+import relational.Adapter
 
 trait Helpers extends ShouldMatchers {
   val name = new Attribute(new Table("examples"), "name")
+  val age = new Attribute(new Table("examples"), "age")
 
-  def pseudoSQL(a: AttributeLike) = a.partial.toPseudoSQL
+  def pseudoSQL(a: AttributeLike)(implicit ad: Adapter) = a.partial.toPseudoSQL
 }

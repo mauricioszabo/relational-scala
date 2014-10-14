29d18c60780f4d8d61084634d52c8893c8ba2cde
Maurício Szabo <mauricio.szabo@gmail.com>
2014-10-14 11:02:19 -0300

    Changed Literal to case class.

--- src/main/scala/relational/Literal.scala
+++ src/main/scala/relational/Literal.scala
@@ -2,6 +2,6 @@ package relational.attributes
 
 import relational.PartialStatement
 
-class Literal(any: Any) extends Comparable {
+case class Literal(any: Any) extends Comparable {
   lazy val partial = PartialStatement(a => "?" -> List(any))
 }

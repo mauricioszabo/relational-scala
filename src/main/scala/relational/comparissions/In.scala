88e82748ecfaa4a53714f112c812473b0ee27122
Maurício Szabo <mauricio.szabo@gmail.com>
2014-10-14 11:21:17 -0300

    Added classes for NotIn and NotNull.

--- src/main/scala/relational/comparissions/In.scala
+++ src/main/scala/relational/comparissions/In.scala
@@ -8,3 +8,8 @@ case class In(attribute: AttributeLike, list: Seq[Any]) extends Comparission {
   lazy val partial = for(a <- attribute.partial)
   yield a.query + " IN (" + list.map(_=>"?").mkString(",") + ")" -> (a.params ++ list)
 }
+
+case class NotIn(attribute: AttributeLike, list: Seq[Any]) extends Comparission {
+  lazy val partial = for(a <- attribute.partial)
+  yield a.query + " NOT IN (" + list.map(_=>"?").mkString(",") + ")" -> (a.params ++ list)
+}
--- src/main/scala/relational/comparissions/IsNull.scala
+++ src/main/scala/relational/comparissions/IsNull.scala
@@ -3,6 +3,10 @@ package relational.comparissions
 import relational.attributes.AttributeLike
 import relational.PartialStatement
 
-class IsNull(attr: AttributeLike) extends Comparission {
+case class IsNull(attr: AttributeLike) extends Comparission {
   lazy val partial = for(a <- attr.partial) yield a.query + " IS NULL" -> a.params
+}
+
+case class NotNull(attr: AttributeLike) extends Comparission {
+  lazy val partial = for(a <- attr.partial) yield a.query + " IS NOT NULL" -> a.params
 }
